20b3b2e77905e168a92b50e2909792ce062db09d
Durgesh Jaiswal <86183313+Jdurgesh86@users.noreply.github.com>
2023-06-22 19:50:36 +0530

    Update Que13.c

--- Que13.c
+++ Que13.c
@@ -1,1 +1,37 @@
-
+#include<stdio.h>
+int main()
+{
+    int i,j,row;
+    printf("Enter the row number ");
+    scanf("%d",&row);
+    for(int i=0;i<(row*2)-1;i++)
+    {
+        char k='A';
+        for(int j=0;j<row;j++)
+        {
+            if(j<(row-i))
+            {
+                printf("%c",k);
+                k++;
+            }
+            else
+                printf(" ");
+        }
+        k='A';
+        int temp=0;
+        if(i>=1)
+            temp=i-1;
+        k+=(row-temp-2);
+        for(int j=0;j<row-1;j++)
+        {
+            if(j<temp)
+                printf(" ");
+            else
+                {
+                printf("%c",k);
+                k--;
+            }
+        }
+        printf("\n");
+    }
+}
