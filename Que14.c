db633a649adc3c7d0424dd4f2738403b79034483
Durgesh Jaiswal <86183313+Jdurgesh86@users.noreply.github.com>
2023-06-23 19:43:44 +0530

    Update Que14.c

--- Que14.c
+++ Que14.c
@@ -1,1 +1,23 @@
 
+#include<stdio.h>
+int main()
+{
+    int i,j,row;
+    printf("Enter the row number :");
+    scanf("%d",&row);
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<row;j++)
+        {
+            if(i==row-1&&j<row)
+                printf("*");
+            else if(i==j||j==0)
+            {
+                printf("*");
+            }
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
