fbac7905346c7f6dd4d1629c1c3217e3c80136ce
Durgesh Jaiswal <86183313+Jdurgesh86@users.noreply.github.com>
2023-06-24 19:49:54 +0530

    Update Que15.c

--- Que15.c
+++ Que15.c
@@ -1,1 +1,25 @@
 
+#include<stdio.h>
+int main()
+{
+    int i,j,row;
+    printf("Enter the row number ");
+    scanf("%d",&row);
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<row;j++)
+        {
+            if(j==(row-1) && i<row)
+                printf("*");
+            else if(i==(row-1) && j<row)
+                    printf("*");
+                    else if((row-1-i)==j)
+                    printf("*");
+                else
+                    printf(" ");
+            
+            
+        }
+        printf("\n");
+    }
+}
